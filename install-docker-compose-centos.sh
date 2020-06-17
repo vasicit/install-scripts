065f5ed423ed3c56e83585fbd0eac54550162c67
Vladimir Vasic <vladimir@vasic.io>
2020-06-17 12:20:41 -0700

    Create install-docker-compose-centos.sh

---
+++ install-docker-compose-centos.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+                  
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+yum -y update
+
+yum --nobest install docker-ce docker-ce-cli containerd.io
+
+systemctl enable docker
+systemctl start docker
+
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
