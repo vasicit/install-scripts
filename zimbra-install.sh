12c6ec092146c470f26346e23e0d1ce9b7085973
Vladimir Vasic <vladimir@vasic.io>
2020-08-09 15:41:37 -0700

    Update zimbra-install.sh

--- zimbra-install.sh
+++ zimbra-install.sh
@@ -31,5 +31,14 @@ dnf -y install wget
 # echo "IP FQDN" > /etc/hosts
 systemctl restart NetworkManager
 
+# Redirect HTTP to HTTPS in Zimbra
+zmprov ms `zmhostname` zimbraReverseProxyMailMode redirect
 
+# Install Certbot
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf install certbot
+wget https://github.com/YetOpen/certbot-zimbra/raw/master/certbot_zimbra.sh
+chmod +x certbot_zimbra.sh
+chown root: certbot_zimbra.sh
+mv certbot_zimbra.sh /usr/local/bin/
 
