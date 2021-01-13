76a14a9b2c92987a4f08df234767a7f2fa0ddf5a
Vladimir Vasic <vladimir@vasic.io>
2021-01-13 04:49:17 -0800

    Update install-docker-compose-debian.sh

--- install-docker-compose-debian.sh
+++ install-docker-compose-debian.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Update Debian
 apt-get -y update && apt-get -y upgrade
 
@@ -26,3 +28,7 @@ apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
 
 # Verify successful Docker engine installation with HelloWorld
 docker run hello-world
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
