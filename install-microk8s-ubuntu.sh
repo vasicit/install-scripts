31d76039c592b9e4e4d81d22d6d9d1b4a31f8cc1
Vladimir Vasic <vladimir@vasic.io>
2021-04-25 11:25:55 +0200

    Update install-microk8s-ubuntu.sh

--- install-microk8s-ubuntu.sh
+++ install-microk8s-ubuntu.sh
@@ -10,10 +10,7 @@ else
     snap install microk8s --classic
     microk8s enable dns dashboard storage ingress
     find /home/ -type d -exec touch {}/.bash_aliases \;
-    for folder in /home/
-    do  
-      echo 'alias kubectl="sudo microk8s kubectl"' >> $folder/.bash_aliases
-      echo 'alias kc="sudo microk8s kubectl"' >> $folder/.bash_aliases
-    done
+    find /home/ -type d -exec echo 'alias kubectl="sudo microk8s kubectl"' >> {}/.bash_aliases \;
+    find /home/ -type d -exec echo 'alias kc="sudo microk8s kubectl"' >> {}/.bash_aliases \;
     touch /etc/ready
 fi
