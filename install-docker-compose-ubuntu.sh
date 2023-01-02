3a66d368f92f630656606597e89c6bca141f0c45
Vladimir Vasic <vladimir@vasic.io>
2023-01-02 07:48:43 +0100

    Create install-docker-compose-ubuntu.sh

---
+++ install-docker-compose-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Update the apt package index and install packages to allow apt to use a repository over HTTPS
+apt-get update
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+#Add Docker’s official GPG key
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#Set up the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+#Update the apt package index
+apt-get update
+
+#Install Docker Engine, containerd, and Docker Compose
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
